e28a1d7b604e64a845047b5bc8eea49c7e99792d
WmmyS <wesleymarcosm@gmail.com>
2022-04-13 15:13:26 -0300

    feat: Criados formulários de login e cadastro.

--- routes.js
+++ routes.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const route = express.Router();
-const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController');
 
-// O next serve para o middleware prosseguir para o próximo middleware
-function meuMiddleware(req, res, next) {
-    console.log();
-    console.log('Passei no seu middleware');
-    console.log();
-    next();
-}
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
 
 // Rotas do home
-route.get('/', meuMiddleware, homeController.paginaInicial);
-route.post('/', homeController.trataPost);
+route.get('/',homeController.index);
 
-//Rotas de contato
-route.get('/contato', contatoController.paginaInicial);
+// Rotas de login
+route.get('/login/index', loginController.index);
 
 module.exports = route;
